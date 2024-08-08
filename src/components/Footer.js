32e632dec07630ddb9132753a45f0b1b0fd12fb7
grkmpasaoglu <gorkempasaoglu@outlook.com>
2024-08-08 23:07:51 +0300

    header component fixed

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,18 +1,3 @@
-// import React from "react";
-
-// const Footer = () => {
-//   return (
-//     <footer className="bottom-0 left-0 p-8 flex items-center text-custom-white syne-header">
-//       <i className="fas fa-map-marker-alt text-sm xl:text-2xl mr-2"></i>
-//       <p className="text-sm xl:text-lg mr-5">ANKARA, TÜRKİYE</p>
-//     </footer>
-//   );
-// };
-
-// export default Footer;
-
-
-
 import React from "react";
 
 const Footer = () => {
